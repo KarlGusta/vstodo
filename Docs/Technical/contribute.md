49c6519a9cb4868e6c60fde446bb64568b0d8d37
Annoh Karlgusta <annoh.karlgusta@gmail.com>
2022-05-09 12:48:49 +0300

    Add: Initial files.
    Adding the initial files.

---
+++ Docs/Technical/contribute.md
@@ -0,0 +1,15 @@
+# How to contribute
+
+For the VS Code template, we use this tutorial to generate it.
+
+[Your First Extension.](https://code.visualstudio.com/api/get-started/your-first-extension)
+
+`F5` to run the debugger. This runs our extension.
+
+`yarn watch` is going to compile our extension.ts to the /dist folder. Webpack transpiles your code. It takes our extension.ts and turns it to extension.js
+
+`ctrl + shift + p` on our extension window, you can run a command of the extension.
+
+Here is an example of the image.
+![Test image](Docs/../../Images/goat.jpeg)
+
--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,25 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
-import { HelloWorldPanel } from "./HelloWorldPanel";
+import * as vscode from 'vscode';
 
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "vstodo" is now active!');
+	
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "vstodo" is now active!');
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand("vstodo.helloWorld", () => {
-      vscode.window.showInformationMessage("Hello from VSTodo!");
-      HelloWorldPanel.createOrShow(context.extensionUri);
-    })
-  );
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('vstodo.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from VSTodo!');
+	});
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand("vstodo.askQuestion", () => {
-      vscode.window.showInformationMessage("How was your day?", "good", "bad");
-    })
-  );
+	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
