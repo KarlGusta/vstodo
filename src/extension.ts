0564866f08cc09f44661adb2aebca8c0d54366a6
Annoh Karlgusta <annoh.karlgusta@gmail.com>
2022-05-12 13:00:38 +0300

    Adding askQuestion command.

--- src/extension.ts
+++ src/extension.ts
@@ -16,10 +16,14 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('vstodo.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VSTodo!');
+		vscode.window.showInformationMessage('Hello from VSTodo!');
 	});
 
 	context.subscriptions.push(disposable);
+
+	vscode.commands.registerCommand('vstodo.askQuestion', () => {
+
+	});
 }
 
 // this method is called when your extension is deactivated
